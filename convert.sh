7865d84da4c4887afd5de6fa72d2e3d79b424b32
Anthony Seure <anthony.seure@gmail.com>
2020-03-09 16:56:24 +0100

    chore: fix Python invocation for Travis build

--- convert.sh
+++ convert.sh
@@ -4,6 +4,14 @@ set -e
 TEXTBUNDLES_DIR='textbundles'
 IFS=$(echo -en "\n\b")
 
+if [ -x "$(command -v python3.8)" ]; then
+  PYTHON='python3.8'
+elif [ -x "$(command -v python3)" ]; then
+  PYTHON='python3'
+else
+  PYTHON='python'
+fi
+
 for b in $(ls $TEXTBUNDLES_DIR); do
-  python3.8 convert_textbundle.py "$TEXTBUNDLES_DIR/$b"
+  $PYTHON convert_textbundle.py "$TEXTBUNDLES_DIR/$b"
 done
