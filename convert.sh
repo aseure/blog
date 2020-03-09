cff3b2d4cebe5d3142aab5dc5ab2d0be2ecc0e4d
Anthony Seure <anthony.seure@gmail.com>
2020-03-09 13:27:28 +0100

    chore: fix python version for script builds

--- convert.sh
+++ convert.sh
@@ -5,5 +5,5 @@ TEXTBUNDLES_DIR='textbundles'
 IFS=$(echo -en "\n\b")
 
 for b in $(ls $TEXTBUNDLES_DIR); do
-  python3 convert_textbundle.py "$TEXTBUNDLES_DIR/$b"
+  python3.8 convert_textbundle.py "$TEXTBUNDLES_DIR/$b"
 done
