de2ab3fe74634551e5c071c4cbebd8d905693962
Anthony Seure <anthony.seure@gmail.com>
2020-06-02 17:24:08 +0200

    chore: fix article generation script

---
+++ content/articles/_index.md
@@ -0,0 +1,6 @@
+---
+title: "Articles"
+layout: "list-articles"
+description: "List of all blog articles."
+weight: 1
+---
--- convert_textbundle.py
+++ convert_textbundle.py
@@ -31,8 +31,8 @@ if len(sys.argv) != 2:
     print('Usage: ./blogit.sh <INPUT_TEXT_BUNDLE>')
     sys.exit(1)
 
-input_textbundle      = os.path.expanduser(sys.argv[1])
-output_hugo_posts_dir = os.path.expanduser('content/posts/')
+input_textbundle         = os.path.expanduser(sys.argv[1])
+output_hugo_articles_dir = os.path.expanduser('content/articles/')
 
 if input_textbundle.endswith('.textpack'):
     with zipfile.ZipFile(input_textbundle, 'r') as f:
@@ -44,8 +44,8 @@ if not dir_exists(input_textbundle):
     print(f'Input TextBundle {input_textbundle} does not exist')
     sys.exit(1)
 
-if not dir_exists(output_hugo_posts_dir):
-    os.makedirs(output_hugo_posts_dir)
+if not dir_exists(output_hugo_articles_dir):
+    os.makedirs(output_hugo_articles_dir)
 
 if not input_textbundle.endswith('.textbundle'):
     print(f'Input Textbundle {input_textbundle} does not have a .textbundle extension')
@@ -58,13 +58,13 @@ article_name          = date_article_name[13:]
 check_valid_date(date)
 
 article_filename      = re.sub(r' ', '-', (date + '-' + article_name).lower())
-output_post_markdown  = os.path.join(output_hugo_posts_dir, article_filename + '.md')
-output_post_dir       = os.path.join(output_hugo_posts_dir, article_filename)
+output_article_markdown  = os.path.join(output_hugo_articles_dir, article_filename + '.md')
+output_article_dir       = os.path.join(output_hugo_articles_dir, article_filename)
 
 print(f':: {article_name}')
 
-remove_file(output_post_markdown)
-remove_dir(output_post_dir)
+remove_file(output_article_markdown)
+remove_dir(output_article_dir)
 
 if file_exists(os.path.join(input_textbundle, 'text.markdown')):
     os.rename(
@@ -74,7 +74,7 @@ if file_exists(os.path.join(input_textbundle, 'text.markdown')):
 
 shutil.copy(
     os.path.join(input_textbundle, 'text.md'),
-    output_post_markdown,
+    output_article_markdown,
 )
 
 input_assets_dir = os.path.join(input_textbundle, 'assets')
@@ -82,10 +82,10 @@ input_assets_dir = os.path.join(input_textbundle, 'assets')
 if dir_exists(input_assets_dir):
     shutil.copytree(
         input_assets_dir,
-        os.path.join(output_post_dir, 'assets'),
+        os.path.join(output_article_dir, 'assets'),
     )
 
-with open(output_post_markdown, 'r+') as f:
+with open(output_article_markdown, 'r+') as f:
     title = next(f)[2:]
     content = f.read()
 
@@ -94,6 +94,6 @@ front_matter = ('---'             +  '\n' +
                'date:  "' + date  + '"\n' +
                '---\n\n')
 
-with open(output_post_markdown, 'w+') as f:
+with open(output_article_markdown, 'w+') as f:
     f.write(front_matter)
     f.write(content)
